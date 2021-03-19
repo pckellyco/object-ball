2c721fd6d77e771cc26d91edd39a3e2d5909f33c
Paige Kelly <pckelly.co@gmail.com>
2021-03-19 14:57:55 -0600

    added gameObject()

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -4,6 +4,130 @@
 //   * Think about where in the object you will find a player's `points`. How can
 //     you iterate down into that level? Think about the return value of your
 //     function.
+
+const gameObject = () => {
+    return {
+        home: {
+            teamName: 'Brooklyn Nets',
+            colors: ['Black', 'White'],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7
+                },
+                "Brooke Lopez": {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
+                },
+                "Mason Plumlee": {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
+                },
+                
+                "Jason Terry": {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 1
+                }
+            }
+        },
+        away: {
+            teamName: 'Charlotte Hornets',
+            colors: ['Turquoise', 'Purple'],
+            players: {
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2 
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5 
+                },
+                "Ben Gordan": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0 
+                },
+                "Brendan Haywood": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12 
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
 // * Build a function, `shoeSize`, that takes in an argument of a player's name and returns the shoe size for that player.
 //   * Think about how you will find the shoe size of the correct player. How can
 //     you check and see if a player's name matches the name that has been passed
