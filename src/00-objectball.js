8946c58f1748ac1a5ec8a0930d1944e002f48a4f
Paige Kelly <pckelly.co@gmail.com>
2021-03-19 14:49:16 -0600

    renamed original solutiion as old-objectball.js and created new file w/ instructions, labeled 00-objectball.js

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,362 +1,55 @@
-//using global variables is bad practice, this takes up more memory is much more efficient
-// create multiple funcitons so thjat they are doing one thing at a time
-// use new methods like forEach, map, filter, find, reduce
+// ### Function Building
 
-const alanAndersonStats =
-    {
-    number: 0,
-    shoe: 16,
-    points: 22,
-    rebounds: 12,
-    assists: 12,
-    steals: 3,
-    blocks: 1,
-    slamDunks: 1
-    };
+// * Build a function, `numPointsScored` that takes in an argument of a player's name and returns the number of points scored for that player.
+//   * Think about where in the object you will find a player's `points`. How can
+//     you iterate down into that level? Think about the return value of your
+//     function.
+// * Build a function, `shoeSize`, that takes in an argument of a player's name and returns the shoe size for that player.
+//   * Think about how you will find the shoe size of the correct player. How can
+//     you check and see if a player's name matches the name that has been passed
+//     into the function as an argument?
+// * Build a function, `teamColors`, that takes in an argument of the team name
+//   and returns an array of that teams colors.
+// * Build a function, `teamNames`, that operates on the game object to return an
+//   array of the team names.
+// * Build a function, `playerNumbers`, that takes in an argument of a team name
+//   and returns an array of the jersey number's for that team.
+// * Build a function, `playerStats`, that takes in an argument of a player's name
+//   and returns a object of that player's stats.
+//   * Check out the following example of the expected return value of the `playerStats` function:
 
-const reggieEvansStats = 
-    {
-    number: 30,
-    shoe: 14,
-    points: 12,
-    rebounds: 12,
-    assists: 12,
-    steals: 12,
-    blocks: 12,
-    slamDunks: 7
-    };
-
-const brookLopezStats =
-    {
-    number: 11,
-    shoe: 17,
-    points: 17,
-    rebounds: 19,
-    assists: 10,
-    steals: 3,
-    blocks: 1,
-    slamDunks: 15
-    };
-
-const masonPlumleeStats =
-    {
-    number: 1,
-    shoe: 19,
-    points: 26,
-    rebounds: 12,
-    assists: 6,
-    steals: 3,
-    blocks: 8,
-    slamDunks: 5
-    };
-
-const jasonTerryStats =
-    {
-    number: 31,
-    shoe: 15,
-    points: 19,
-    rebounds: 2,
-    assists: 2,
-    steals: 3,
-    blocks: 8,
-    slamDunks: 1 
-    };
-
-const jeffAdrienStats =
-    {
-    number: 4,
-    shoe: 18,
-    points: 10,
-    rebounds: 1,
-    assists: 1,
-    steals: 2,
-    blocks: 7,
-    slamDunks: 2 
-    };
-
-const bismakBiyomboStats =
-    {
-    number: 0,
-    shoe: 16,
-    points: 12,
-    rebounds: 4,
-    assists: 7,
-    steals: 7,
-    blocks: 15,
-    slamDunks: 10
-    };
-
-const deSagnaDiopStats =
-    {
-    number: 2,
-    shoe: 14,
-    points: 24,
-    rebounds: 12,
-    assists: 12,
-    steals: 4,
-    blocks: 5,
-    slamDunks: 5 
-    };
-
-const benGordanStats =
-    {
-    number: 8,
-    shoe: 15,
-    points: 33,
-    rebounds: 3,
-    assists: 2,
-    steals: 1,
-    blocks: 1,
-    slamDunks: 0 
-    };
-
-const brendanHaywoodStats =
-    {
-    number: 33,
-    shoe: 15,
-    points: 6,
-    rebounds: 12,
-    assists: 12,
-    steals: 22,
-    blocks: 5,
-    slamDunks: 12 
-    };
-
-const gameObject = () => {
-    return {    // can just return instead of creating variable, better to create object as function so it doesn't run the whole thing until its called
-        home: {
-            teamName: 'Brooklyn Nets',
-            colors: ['Black', 'White'],
-            players: {
-                "Alan Anderson": alanAndersonStats,
-                "Reggie Evans": reggieEvansStats,
-                "Brooke Lopez": brookLopezStats,
-                "Mason Plumlee": masonPlumleeStats,
-                "Jason Terry": jasonTerryStats
-            }
-        },
-        away: {
-            teamName: 'Charlotte Hornets',
-            colors: ['Turquoise', 'Purple'],
-            players: {
-                "Jeff Adrien": jeffAdrienStats,
-                "Bismak Biyombo": bismakBiyomboStats,
-                "DeSagna Diop": deSagnaDiopStats,
-                "Ben Gordan": benGordanStats,
-                "Brendan Haywood": brendanHaywoodStats
-            }
-        }
-    }
-}
-
-const gameObj = gameObject();
-
-
-//takes in players name, returns their score
-//need to check home and away to match name, then return points
-//maybe helper function
-
-// Given a player's name, return the number of points scored by that player
-// function numPointsScored(playerInput) {
-//     const game = gameObject()
-//     debugger
-//     for (let gameKey in game) { // iterate through game keys (home, away)
-//         let teamObj = game[gameKey]
-//         debugger
-//         for (let teamKey in teamObj) { // iterate through team keys (teamName, colors, players)
-//             let playerObj = teamObj.players
-//             debugger
-//             for (let playerKey in playerObj) { // iterate through players
-//                 debugger
-//                 if (playerKey === playerInput) { // check to see if player matches function argument
-//                     return playerObj[playerKey].points // return the player's points
-//                 }
-//             }
-//         }
+//     ```js
+//     playerStats("Alan Anderson")
+//     {
+//       number: 0,
+//       shoe: 16,
+//       points: 22,
+//       rebounds: 12,
+//       assists: 12,
+//       steals: 3,
+//       blocks: 1,
+//       slamDunks: 1
 //     }
-// }
+//     ```
 
-// debugger
-
-const players = () => {
-    const homePlayers = gameObj.home.players;
-    const awaysPlayers = gameObj.
-    Object.assign({}, gameObj.home.players, gameObj.away.players)
-}
-console.log('players', players())
-
-const numPointsScored = (name) => {
-    if (gameObj.home.players[name] !== undefined) {
-        return gameObj.home.players[name].points
-    } else if (gameObj.away.players[name] !== undefined) {
-        return gameObj.away.players[name].points
-    } else {
-        return "Player was not in the game."
-    }
-}
-
-// console.log(numPointsScored("Jeff Adrien"));
-
-function shoeSize(name) {
-    if (gameObj.home.players[name] !== undefined) {
-        return gameObj.home.players[name].shoe;
-    } else if (gameObj.away.players[name] !== undefined) {
-        return gameObj.away.players[name].shoe;
-    } else {
-        return "Player was not in the game.";
-    }
-}
-
-// console.log(shoeSize("Jason Terry")); --- check
-
-function teamColors(team) {
-    if (gameObj.home.teamName === team) {
-        return gameObj.home.colors;
-    } else if (gameObj.away.teamName === team) {
-        return gameObj.away.colors;
-    } else {
-        return "Team did not play in the game.";
-    }
-}
-
-// console.log(teamColors('Brooklyn Nets')); --- check
-
-function teamNames(object) {
-    return [object.home.teamName, object.away.teamName]
-}
-
-// console.log(teamNames(gameObj)); --- check
-
-// check if team name is away or home, then iterate through the object to get playerNums
-// use teamNames to check if team is away or home
-
-function homeOrAwayCheck(nameOfTeam) {  //created helper  function to check if team is home or away
-    if (gameObj.home.teamName === nameOfTeam) {
-        return 'home';
-    } else if (gameObj.away.teamName === nameOfTeam) {
-        return 'away'
-    }
-}
-
-function playerNumbers(nameOfTeam) {
-    const playersNumbers = []
-    const teamLocation = homeOrAwayCheck(nameOfTeam);
-    const playersObj = gameObj[teamLocation].players;
-    for (let playerNameKey in playersObj) {
-        let player = playersObj[playerNameKey]
-        playersNumbers.push(player.number)
-    }
-
-    return playersNumbers;
-}
-
-// console.log(playerNumbers('Brooklyn Nets')) --- check
+// * Build a function, `bigShoeRebounds`, that will return the number of rebounds
+//   associated with the player that has the largest shoe size. Break this one
+//   down into steps:
+//   * First, find the player with the largest shoe size
+//   * Then, return that player's number of rebounds
+//   * Remember to think about return values here. Use `debugger` to drop into
+//     your function and understand what it is returning and why.
 
 
-function playerTeamLocation(playerName) {
-    if (gameObj.home.players[playerName] !== undefined) {
-        return 'home';
-    } else if (gameObj.away.players[playerName] !== undefined) {
-        return 'away';
-    }
-}
+// **Bonus Questions:**
 
-// console.log(playerTeamLocation('Alan Anderson')) --- check
+// Define functions to return the answer to the following questions:
 
-function playerStats(playerName) {
-    const playerTeam = playerTeamLocation(playerName);
-    return gameObj[playerTeam].players[playerName];
-}
+// 1. Which player has the most points? Call the function `mostPointsScored`.
+// 2. Which team has the most points? Call the function `winningTeam`.
+// 3. Which player has the longest name? Call the function `playerWithLongestName`.
 
-// console.log(playerStats('Bismak Biyombo')); ---check
-
-// use numPointsScored which returns points scored by a player
-// create variables to store points
-
-
-const homePlayersObj = gameObj.home.players // returns object, players names are the keys
-const awayPlayersObj = gameObj.away.players
-
-const mostPointsScored = () => {
-    let bestPlayer = '';
-    for (let homePlayer in homePlayersObj) {
-        const currentHomePlayerPoints = numPointsScored(homePlayer)
-        if (currentHomePlayerPoints > numPointsScored(bestPlayer) || bestPlayer === '') {
-            bestPlayer = homePlayer;
-            bestPlayerLocation = 'home'
-        }
-    }
-    for (let awayPlayer in awayPlayersObj) {
-        const currentAwayPlayerPoints = numPointsScored(awayPlayer)
-        if (currentAwayPlayerPoints > numPointsScored(bestPlayer) || bestPlayer === '') {
-            bestPlayer = awayPlayer;
-            bestPlayerLocation = 'away'
-        }
-    }
-
-    return numPointsScored(bestPlayer);
-}
-
-console.log(mostPointsScored())
-
-const winningTeam= () => {
-    let homePoints = 0;
-    let awayPoints = 0;
-    for (let homePlayer in homePlayersObj) {
-        const homePlayerPoints = numPointsScored(homePlayer)
-        homePoints += homePlayerPoints
-    }
-    for (let awayPlayer in awayPlayersObj) {
-        const awayPlayerPoints = numPointsScored(awayPlayer)
-        awayPoints += awayPlayerPoints
-    }
-    if (homePoints > awayPoints) {
-        return gameObj.home.teamName;
-    } else {
-        return gameObj.away.teamName;
-    }
-}
-
-console.log(winningTeam())
-
-
-const playerWithLongestName = () => {
-    let longestName = '';
-    for (let homePlayer in homePlayersObj) {
-        if (homePlayer.length > longestName.length) {
-            longestName = homePlayer;
-        }
-    }
-    for (let awayPlayer in awayPlayersObj) {
-        if (awayPlayer.length > longestName.length) {
-            longestName = awayPlayer
-        }
-    }
-
-    return longestName;
-}
-
-console.log(playerWithLongestName())
-
+// **Super Bonus:**
 
 // 1. Write a function that returns true if the player with the longest name had
 //    the most steals. Call the function `doesLongNameStealATon`.
-// create a helper function to determine person who steals the most, then compare it to longestName steals
-const doesLongNameStealATon = () => {
-    const longestNameSteals = playerStats(playerWithLongestName()).steals
-    for (let homePlayer in homePlayersObj) {
-        if (playerStats(homePlayer).steals > longestNameSteals) {
-            return false;
-        }
-    }
-    for (let awayPlayer in awayPlayersObj) {
-        if (playerStats(awayPlayer).steals > longestNameSteals) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-console.log(doesLongNameStealATon())
-
---
+++ src/old-objectball.js
@@ -0,0 +1,371 @@
+//using global variables is bad practice, this takes up more memory is much more efficient
+// create multiple funcitons so thjat they are doing one thing at a time
+// use new methods like forEach, map, filter, find, reduce
+// index works as keys in arrays
+// nums = [0, 1, 2, 3]
+// nums = {
+//     '0': 1,
+//     '1': 2,
+//     '2': 3,
+//     '3': 4
+// }
+// parameters are only available in context within function because of scope so functions can take in a parameter in functions
+
+const alanAndersonStats =
+    {
+    number: 0,
+    shoe: 16,
+    points: 22,
+    rebounds: 12,
+    assists: 12,
+    steals: 3,
+    blocks: 1,
+    slamDunks: 1
+    };
+
+const reggieEvansStats = 
+    {
+    number: 30,
+    shoe: 14,
+    points: 12,
+    rebounds: 12,
+    assists: 12,
+    steals: 12,
+    blocks: 12,
+    slamDunks: 7
+    };
+
+const brookLopezStats =
+    {
+    number: 11,
+    shoe: 17,
+    points: 17,
+    rebounds: 19,
+    assists: 10,
+    steals: 3,
+    blocks: 1,
+    slamDunks: 15
+    };
+
+const masonPlumleeStats =
+    {
+    number: 1,
+    shoe: 19,
+    points: 26,
+    rebounds: 12,
+    assists: 6,
+    steals: 3,
+    blocks: 8,
+    slamDunks: 5
+    };
+
+const jasonTerryStats =
+    {
+    number: 31,
+    shoe: 15,
+    points: 19,
+    rebounds: 2,
+    assists: 2,
+    steals: 3,
+    blocks: 8,
+    slamDunks: 1 
+    };
+
+const jeffAdrienStats =
+    {
+    number: 4,
+    shoe: 18,
+    points: 10,
+    rebounds: 1,
+    assists: 1,
+    steals: 2,
+    blocks: 7,
+    slamDunks: 2 
+    };
+
+const bismakBiyomboStats =
+    {
+    number: 0,
+    shoe: 16,
+    points: 12,
+    rebounds: 4,
+    assists: 7,
+    steals: 7,
+    blocks: 15,
+    slamDunks: 10
+    };
+
+const deSagnaDiopStats =
+    {
+    number: 2,
+    shoe: 14,
+    points: 24,
+    rebounds: 12,
+    assists: 12,
+    steals: 4,
+    blocks: 5,
+    slamDunks: 5 
+    };
+
+const benGordanStats =
+    {
+    number: 8,
+    shoe: 15,
+    points: 33,
+    rebounds: 3,
+    assists: 2,
+    steals: 1,
+    blocks: 1,
+    slamDunks: 0 
+    };
+
+const brendanHaywoodStats =
+    {
+    number: 33,
+    shoe: 15,
+    points: 6,
+    rebounds: 12,
+    assists: 12,
+    steals: 22,
+    blocks: 5,
+    slamDunks: 12 
+    };
+
+const gameObject = () => {
+    return {    // can just return instead of creating variable, better to create object as function so it doesn't run the whole thing until its called
+        home: {
+            teamName: 'Brooklyn Nets',
+            colors: ['Black', 'White'],
+            players: {
+                "Alan Anderson": alanAndersonStats,
+                "Reggie Evans": reggieEvansStats,
+                "Brooke Lopez": brookLopezStats,
+                "Mason Plumlee": masonPlumleeStats,
+                "Jason Terry": jasonTerryStats
+            }
+        },
+        away: {
+            teamName: 'Charlotte Hornets',
+            colors: ['Turquoise', 'Purple'],
+            players: {
+                "Jeff Adrien": jeffAdrienStats,
+                "Bismak Biyombo": bismakBiyomboStats,
+                "DeSagna Diop": deSagnaDiopStats,
+                "Ben Gordan": benGordanStats,
+                "Brendan Haywood": brendanHaywoodStats
+            }
+        }
+    }
+}
+
+const gameObj = gameObject();
+
+
+//takes in players name, returns their score
+//need to check home and away to match name, then return points
+//maybe helper function
+
+// Given a player's name, return the number of points scored by that player
+// function numPointsScored(playerInput) {
+//     const game = gameObject()
+//     debugger
+//     for (let gameKey in game) { // iterate through game keys (home, away)
+//         let teamObj = game[gameKey]
+//         debugger
+//         for (let teamKey in teamObj) { // iterate through team keys (teamName, colors, players)
+//             let playerObj = teamObj.players
+//             debugger
+//             for (let playerKey in playerObj) { // iterate through players
+//                 debugger
+//                 if (playerKey === playerInput) { // check to see if player matches function argument
+//                     return playerObj[playerKey].points // return the player's points
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// debugger
+
+const players = () => {
+    const homePlayers = gameObj.home.players;
+    const awaysPlayers = gameObj.
+    Object.assign({}, gameObj.home.players, gameObj.away.players)
+}
+console.log('players', players())
+
+const numPointsScored = (name) => {
+    if (gameObj.home.players[name] !== undefined) {
+        return gameObj.home.players[name].points
+    } else if (gameObj.away.players[name] !== undefined) {
+        return gameObj.away.players[name].points
+    } else {
+        return "Player was not in the game."
+    }
+}
+
+// console.log(numPointsScored("Jeff Adrien"));
+
+function shoeSize(name) {
+    if (gameObj.home.players[name] !== undefined) {
+        return gameObj.home.players[name].shoe;
+    } else if (gameObj.away.players[name] !== undefined) {
+        return gameObj.away.players[name].shoe;
+    } else {
+        return "Player was not in the game.";
+    }
+}
+
+// console.log(shoeSize("Jason Terry")); --- check
+
+function teamColors(team) {
+    if (gameObj.home.teamName === team) {
+        return gameObj.home.colors;
+    } else if (gameObj.away.teamName === team) {
+        return gameObj.away.colors;
+    } else {
+        return "Team did not play in the game.";
+    }
+}
+
+// console.log(teamColors('Brooklyn Nets')); --- check
+
+function teamNames(object) {
+    return [object.home.teamName, object.away.teamName]
+}
+
+// console.log(teamNames(gameObj)); --- check
+
+// check if team name is away or home, then iterate through the object to get playerNums
+// use teamNames to check if team is away or home
+
+function homeOrAwayCheck(nameOfTeam) {  //created helper  function to check if team is home or away
+    if (gameObj.home.teamName === nameOfTeam) {
+        return 'home';
+    } else if (gameObj.away.teamName === nameOfTeam) {
+        return 'away'
+    }
+}
+
+function playerNumbers(nameOfTeam) {
+    const playersNumbers = []
+    const teamLocation = homeOrAwayCheck(nameOfTeam);
+    const playersObj = gameObj[teamLocation].players;
+    for (let playerNameKey in playersObj) {
+        let player = playersObj[playerNameKey]
+        playersNumbers.push(player.number)
+    }
+
+    return playersNumbers;
+}
+
+// console.log(playerNumbers('Brooklyn Nets')) --- check
+
+
+function playerTeamLocation(playerName) {
+    if (gameObj.home.players[playerName] !== undefined) {
+        return 'home';
+    } else if (gameObj.away.players[playerName] !== undefined) {
+        return 'away';
+    }
+}
+
+// console.log(playerTeamLocation('Alan Anderson')) --- check
+
+function playerStats(playerName) {
+    const playerTeam = playerTeamLocation(playerName);
+    return gameObj[playerTeam].players[playerName];
+}
+
+// console.log(playerStats('Bismak Biyombo')); ---check
+
+// use numPointsScored which returns points scored by a player
+// create variables to store points
+
+
+const homePlayersObj = gameObj.home.players // returns object, players names are the keys
+const awayPlayersObj = gameObj.away.players
+
+const mostPointsScored = () => {
+    let bestPlayer = '';
+    for (let homePlayer in homePlayersObj) {
+        const currentHomePlayerPoints = numPointsScored(homePlayer)
+        if (currentHomePlayerPoints > numPointsScored(bestPlayer) || bestPlayer === '') {
+            bestPlayer = homePlayer;
+            bestPlayerLocation = 'home'
+        }
+    }
+    for (let awayPlayer in awayPlayersObj) {
+        const currentAwayPlayerPoints = numPointsScored(awayPlayer)
+        if (currentAwayPlayerPoints > numPointsScored(bestPlayer) || bestPlayer === '') {
+            bestPlayer = awayPlayer;
+            bestPlayerLocation = 'away'
+        }
+    }
+
+    return numPointsScored(bestPlayer);
+}
+
+console.log(mostPointsScored())
+
+const winningTeam= () => {
+    let homePoints = 0;
+    let awayPoints = 0;
+    for (let homePlayer in homePlayersObj) {
+        const homePlayerPoints = numPointsScored(homePlayer)
+        homePoints += homePlayerPoints
+    }
+    for (let awayPlayer in awayPlayersObj) {
+        const awayPlayerPoints = numPointsScored(awayPlayer)
+        awayPoints += awayPlayerPoints
+    }
+    if (homePoints > awayPoints) {
+        return gameObj.home.teamName;
+    } else {
+        return gameObj.away.teamName;
+    }
+}
+
+console.log(winningTeam())
+
+
+const playerWithLongestName = () => {
+    let longestName = '';
+    for (let homePlayer in homePlayersObj) {
+        if (homePlayer.length > longestName.length) {
+            longestName = homePlayer;
+        }
+    }
+    for (let awayPlayer in awayPlayersObj) {
+        if (awayPlayer.length > longestName.length) {
+            longestName = awayPlayer
+        }
+    }
+
+    return longestName;
+}
+
+console.log(playerWithLongestName())
+
+
+// 1. Write a function that returns true if the player with the longest name had
+//    the most steals. Call the function `doesLongNameStealATon`.
+// create a helper function to determine person who steals the most, then compare it to longestName steals
+const doesLongNameStealATon = () => {
+    const longestNameSteals = playerStats(playerWithLongestName()).steals
+    for (let homePlayer in homePlayersObj) {
+        if (playerStats(homePlayer).steals > longestNameSteals) {
+            return false;
+        }
+    }
+    for (let awayPlayer in awayPlayersObj) {
+        if (playerStats(awayPlayer).steals > longestNameSteals) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(doesLongNameStealATon())
+
